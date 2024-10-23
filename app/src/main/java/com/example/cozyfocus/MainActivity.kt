45d0a09cc14e94185063478800261d08be30bc97
GisselleAngelina <gizz.tan@gmail.com>
2024-10-23 22:43:56 +0700

    music

--- app/src/main/java/com/example/cozyfocus/MainActivity.kt
+++ app/src/main/java/com/example/cozyfocus/MainActivity.kt
@@ -1,37 +1,83 @@
 package com.example.cozyfocus
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var playPauseButton: ImageButton
+    private var isPlaying = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstFragment = FirstFragment()
-        val secondFragment = SecondFragment()
-        val thirdFragment = ThirdFragment()
-        val fourthFragment = FourthFragment()
+        // Menambahkan FirstFragment ke dalam fragment container
+        if (savedInstanceState == null) {
+            val firstFragment = FirstFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.flFragment, firstFragment)
+                .commit()
+        }
 
-        setCurrentFragment(firstFragment)
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+        mediaPlayer.isLooping = true
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> setCurrentFragment(firstFragment)
-                R.id.task -> setCurrentFragment(secondFragment)
-                R.id.activity -> setCurrentFragment(thirdFragment)
-                R.id.profile -> setCurrentFragment(fourthFragment)
+        playPauseButton = findViewById(R.id.volume_button)
+        mediaPlayer.start()
+        updateButtonIcon()
+        playPauseButton.setOnClickListener {
+            toggleMusic()
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    val firstFragment = FirstFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.flFragment, firstFragment)
+                        .commit()
+                    true
+                }
+                R.id.task -> {
+                    val secondFragment = SecondFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.flFragment, secondFragment)
+                        .commit()
+                    true
+                }
+                else -> false
             }
-            true
         }
     }
 
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            commit()
+    private fun toggleMusic() {
+        if (isPlaying) {
+            mediaPlayer.pause()
+        } else {
+            mediaPlayer.start()
         }
+        isPlaying = !isPlaying
+        updateButtonIcon()
+    }
+
+    private fun updateButtonIcon() {
+        if (isPlaying) {
+            playPauseButton.setImageResource(R.drawable.music)
+        } else {
+            playPauseButton.setImageResource(R.drawable.nomusic)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::mediaPlayer.isInitialized) {
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+    }
 }
